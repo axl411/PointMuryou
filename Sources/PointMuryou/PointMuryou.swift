ef40a88445dd9211554fce8c640119e5540dff7e
axl411 <axl411511@live.com>
2018-03-13 00:52:03 +0900

    add code from eposide 2

--- Sources/PointMuryou/PointMuryou.swift
+++ Sources/PointMuryou/PointMuryou.swift
@@ -1,19 +1,57 @@
+// MARK: - 0001-functions
+
 precedencegroup ForwardApplication {
     associativity: left
 }
 
 infix operator |>: ForwardApplication
-public func |><A, B>(x: A, f: (A) -> B) -> B {
+
+public func |><A, B>(
+    x: A,
+    f: (A) -> B
+) -> B {
     return f(x)
 }
 
 
 precedencegroup ForwardComposition {
-    higherThan: ForwardApplication
     associativity: right
+    higherThan: ForwardApplication, EffectfulComposition
 }
 
 infix operator >>>: ForwardComposition
-public func >>><A, B, C>(_ f: @escaping (A) -> B, _ g: @escaping (B) -> C) -> ((A) -> C) {
-    return { a in g(f(a)) }
+
+public func >>><A, B, C>(
+    _ f: @escaping (A) -> B,
+    _ g: @escaping (B) -> C
+) -> ((A) -> C) {
+    return { g(f($0)) }
 }
+
+// MARK: - 0002-side-effects
+
+precedencegroup EffectfulComposition {
+    associativity: left
+    higherThan: ForwardApplication
+}
+
+infix operator >=>: EffectfulComposition
+
+public func >=> <A, B, C>(
+    _ f: @escaping (A) -> B?,
+    _ g: @escaping (B) -> C?
+) -> ((A) -> C?) {
+    return { a in
+        return f(a).flatMap { g($0) }
+    }
+}
+
+public func >=> <A, B, C>(
+    _ f: @escaping (A) -> [B],
+    _ g: @escaping (B) -> [C]
+) -> ((A) -> [C]) {
+    return { a in
+        return f(a).flatMap { g($0) }
+    }
+}
+
