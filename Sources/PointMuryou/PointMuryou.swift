64954c67a1afda08b82d36462cd2d4c2d5692000
axl411 <axl411511@live.com>
2018-03-14 00:24:13 +0900

    add <> operator

--- Sources/PointMuryou/PointMuryou.swift
+++ Sources/PointMuryou/PointMuryou.swift
@@ -55,3 +55,39 @@ public func >=> <A, B, C>(
     }
 }
 
+// MARK: - j
+
+precedencegroup SingleTypeComposition {
+    associativity: right
+    higherThan: ForwardApplication
+}
+
+infix operator <>: SingleTypeComposition
+
+public func <> <A>(
+    f: @escaping (A) -> A,
+    g: @escaping (A) -> A
+) -> (A) -> A {
+    return f >>> g
+}
+
+public func <> <A>(
+    f: @escaping (inout A) -> Void,
+    g: @escaping (inout A) -> Void
+) -> (inout A) -> Void {
+    return { a in
+        f(&a)
+        g(&a)
+    }
+}
+
+public func <> <A: AnyObject>(
+    f: @escaping (A) -> Void,
+    g: @escaping (A) -> Void
+) -> (A) -> Void {
+    return { a in
+        f(a)
+        g(a)
+    }
+}
+
